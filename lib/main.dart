28a5a7499492ee0523c309059e4a812318812d86
ashir <ashirza@gmail.com>
2020-08-06 22:43:26 +0100

    added button to go to new route, so that adding games can be done in the new route in the future

--- lib/main.dart
+++ lib/main.dart
@@ -13,54 +13,84 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-        ),
-        body: Center(
-          child: Column(
-            children: [
-              Text('Favourite Games'),
-              Text('TLOU2 : 10'),
-              Text('Zelda : 10'),
-              FlatButton(
-                onPressed: () async {
-                  int i = await DatabaseHelper.instance.insert({
-                    DatabaseHelper.columnName : 'Zelda',
-                    DatabaseHelper.columnRating : 10,
-                  });
-                  print('the inserted id is $i' );
-                },
-                child: Text('Insert'),
-              ),
-              FlatButton(
-                onPressed: () async {
-                  List<Map<String, dynamic>> queryRows = await DatabaseHelper.instance.queryAll();
-                  print(queryRows);
-                },
-                child: Text('Query'),
-              ),
-              FlatButton(
-                onPressed: () async {
-                  int updatedId = await DatabaseHelper.instance.update({
-                    DatabaseHelper.columnId : 2,
-                    DatabaseHelper.columnName : 'TLOU',
-                    DatabaseHelper.columnRating : 10,
-                  });
-                  print(updatedId);
-                },
-                child: Text('Update'),
-              ),
-              FlatButton(
-                onPressed: () async {
-                  int rowsAffected = await DatabaseHelper.instance.delete(2);
-                  print(rowsAffected);
-                },
-                child: Text('Delete'),
-              ),
-            ],
-          ),
+      home: myHome()
+    );
+  }
+}
+
+class myHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text('Favourite Games'),
+            Text('TLOU2 : 10'),
+            Text('Zelda : 10'),
+            FlatButton(
+              onPressed: () async {
+                int i = await DatabaseHelper.instance.insert({
+                  DatabaseHelper.columnName : 'Zelda',
+                  DatabaseHelper.columnRating : 10,
+                });
+                print('the inserted id is $i' );
+              },
+              child: Text('Insert'),
+            ),
+            FlatButton(
+              onPressed: () async {
+                List<Map<String, dynamic>> queryRows = await DatabaseHelper.instance.queryAll();
+                print(queryRows);
+              },
+              child: Text('Query'),
+            ),
+            FlatButton(
+              onPressed: () async {
+                int updatedId = await DatabaseHelper.instance.update({
+                  DatabaseHelper.columnId : 2,
+                  DatabaseHelper.columnName : 'TLOU',
+                  DatabaseHelper.columnRating : 10,
+                });
+                print(updatedId);
+              },
+              child: Text('Update'),
+            ),
+            FlatButton(
+              onPressed: () async {
+                int rowsAffected = await DatabaseHelper.instance.delete(2);
+                print(rowsAffected);
+              },
+              child: Text('Delete'),
+            ),
+          ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(
+              builder: (context) => AddGameRoute()),
+          );
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.green,
+      ),
+    );
+  }
+}
+
+class AddGameRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: RaisedButton(
+          child: Text('Go back'),
+          onPressed: () {Navigator.pop(context);},
+        ),
+      )
     );
   }
 }
